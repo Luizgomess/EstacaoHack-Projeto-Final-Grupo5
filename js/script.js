8e4ea07d9eac64cf223821335f44c6bea6c4b04b
Klausm <klausgalm22@gmail.com>
2020-12-02 14:58:07 -0300

    modais implementadas no index

--- js/script.js
+++ js/script.js
@@ -1,20 +1,35 @@
-document.querySelector('.close-btn').addEventListener('click', () => {
-  document.getElementById('login-modal').style.display = 'none';
-});
+let closeButtonArray = document.querySelectorAll('.close-btn');
+for (const button of closeButtonArray) {
+  button.addEventListener('click', function () {
+    CloseAllModal();
+  })
+}
 
 let loginLinks = document.querySelectorAll('.login-link');
 for (const link of loginLinks) {
-  link.addEventListener('click', function() {
+  link.addEventListener('click', function () {
+    CloseAllModal();
     OpenModal('login-modal', 'flex');
   })
 }
 
 let registerLinks = document.querySelectorAll('.register-link');
 for (const link of registerLinks) {
-  link.addEventListener('click', function() {
+  link.addEventListener('click', function () {
+    CloseAllModal();
     OpenModal('register-modal', 'flex');
   })
 }
+
+document.querySelectorAll('.contact-link')[0].addEventListener('click', function () {
+  CloseAllModal();
+  OpenModal('contact-modal', 'flex');
+})
+
+document.querySelectorAll('.newsletter-link')[0].addEventListener('click', function () {
+  CloseAllModal();
+  OpenModal('newsletter-modal', 'flex');
+})
 
 function OpenModal(modalId, displayValue) {
   let modalElement = document.getElementById(modalId);
@@ -22,5 +37,12 @@ function OpenModal(modalId, displayValue) {
 }
 
 function CloseAllModal() {
-  document.querySelectorAll('.modal').style.display = 'none';
+  // console.log(document.querySelectorAll('.modal'));
+  let modalArray = document.querySelectorAll('.modal');
+  for (const modal of modalArray) {
+    if (modal.style.display != 'none') {
+      modal.style.display = 'none';
+    }
+  }
+  // document.querySelectorAll('.modal').style.display = 'none';
 }
