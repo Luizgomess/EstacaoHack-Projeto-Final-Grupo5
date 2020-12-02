76eedfe7c39dab7c5fc541f65d0572093d21d02c
Klausm <klausgalm22@gmail.com>
2020-12-02 11:39:43 -0300

    adicionando mais javascript

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
-let closeBtn = document.querySelector('.close-btn');
-
-closeBtn.addEventListener('click', () => {
+document.querySelector('.close-btn').addEventListener('click', () => {
   document.getElementById('login-modal').style.display = 'none';
 });
+
+let loginLinks = document.querySelectorAll('.login-link');
+for (const link of loginLinks) {
+  link.addEventListener('click', function() {
+    OpenModal('login-modal', 'flex');
+  })
+}
+
+let registerLinks = document.querySelectorAll('.register-link');
+for (const link of registerLinks) {
+  link.addEventListener('click', function() {
+    OpenModal('register-modal', 'flex');
+  })
+}
+
+function OpenModal(modalId, displayValue) {
+  let modalElement = document.getElementById(modalId);
+  modalElement.style.display = displayValue;
+}
+
+function CloseAllModal() {
+  document.querySelectorAll('.modal').style.display = 'none';
+}
