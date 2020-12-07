8d77abd6fed1c67b274c5bbdbe433d52d20fc646
luiz gomes <luizgomes77777@gmail.com>
2020-12-07 01:33:45 -0300

    carrossel test

--- js/carouselMethod.js
+++ js/carouselMethod.js
@@ -1,9 +1,32 @@
+var body = document.querySelector('body');
 let looks = document.querySelector('#container-looks');
 let width = window.innerWidth;
-var clientWidth = looks.clientWidth;
+var clientWidth = body.clientWidth;
 
     
-if(clientWidth < 900) {
-    looks.classList.toggle('container-carousel')
-    console.log('funciona')
-} 
+function beCarousel() {
+        looks.classList.toggle('container-carousel')
+        console.log('funciona')
+}
+
+
+
+function notCarousel() {
+    looks.classList.remove('container-carousel')
+    console.log('funciona nao carrossel')
+    
+}
+
+function carousel (){
+    if(clientWidth < 900) {
+        beCarousel()
+    } else if (clientWidth > 900) {
+        notCarousel()
+    } else {
+        console.log('deu erro papai')
+    }
+}
+
+
+
+
