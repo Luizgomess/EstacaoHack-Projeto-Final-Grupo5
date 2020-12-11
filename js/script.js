36d8e644b881de3da115278e14e0660f19e24f8c
Klausm <klausgalm22@gmail.com>
2020-12-11 09:35:41 -0300

    animações dos modais

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+const tl = gsap.timeline({ defaults: { ease: "power2.inOut", duration: 1 } })
+const tlClose = gsap.timeline({ defaults: { ease: "power2.inOut", duration: 0.5 } })
+
 let closeButtonArray = document.querySelectorAll('.close-btn');
 for (const button of closeButtonArray) {
   button.addEventListener('click', function () {
@@ -5,62 +8,70 @@ for (const button of closeButtonArray) {
   })
 }
 
-let loginLinks = document.querySelectorAll('.login-link');
-for (const link of loginLinks) {
-  link.addEventListener('click', function () {
-    CloseAllModal();
-    OpenModal('login-modal', 'flex');
-  })
-}
-
 let registerLinks = document.querySelectorAll('.register-link');
 for (const link of registerLinks) {
   link.addEventListener('click', function () {
     CloseAllModal();
-    OpenModal('register-modal', 'flex');
+    let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+    if (vw < 1024) {
+      OpenModalMobile('register-modal');
+    } else {
+      OpenModal('register-modal');
+    }
   })
 }
 
-let contactLinks = document.querySelectorAll('.contact-link');
-for (const link of contactLinks) {
-  link.addEventListener('click', function () {
-    CloseAllModal();
-    OpenModal('contact-modal', 'flex');
-  })
-}
-
-let newsletterLinks = document.querySelectorAll('.newsletter-link');
-for (const link of newsletterLinks) {
-  link.addEventListener('click', function () {
-    CloseAllModal();
-    OpenModal('newsletter-modal', 'flex');
-  })
-}
-
-let aboutLinks = document.querySelectorAll('.about-link');
-for (const link of aboutLinks) {
-  link.addEventListener('click', function () {
-    CloseAllModal();
-    OpenModal('about-modal', 'flex');
-  })
-}
-
-document.querySelectorAll('.menu-btn')[0].addEventListener('click', function () {
+function OpenModalMobile(modalId) {
   CloseAllModal();
-  OpenModal('menu-modal', 'flex');
-})
-
-function OpenModal(modalId, displayValue) {
-  console.log(modalId + displayValue);
   let modalElement = document.getElementById(modalId);
-  modalElement.style.display = displayValue;
+  modalElement.classList.add("show__menu");
+  modalElement.classList.remove("hide__menu");
 }
 
 function CloseAllModal() {
+  let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
   let modalArray = document.querySelectorAll('.modal');
-  for (const modal of modalArray) {
-    if (modal.style.display != 'none') {
-      modal.style.display = 'none';
+  
+  if (vw < 1024) {
+    for (const modal of modalArray) {
+      if (modal.classList.contains('show__menu')) {
+        modal.classList.remove("show__menu");
+        modal.classList.add("hide__menu");
+      }
+    }
+  } else {
+    for (const modal of modalArray) {
+      if (modal.classList.contains('displayed')) {
+        tlClose.to(modal, { backgroundColor: 'rgba(0, 0, 0, 0)' })
+          .to(modal, { x: '2000px' })
+  
+        modal.classList.remove("displayed");
+      }
     }
   }
+}
+
+
+//Animações do Modal Desktop
+function OpenModal(modalId) {
+  CloseAllModal();
+  let modalElement = document.getElementById(modalId);
+  modalElement.classList.add("displayed");
+  if (modalId == 'register-modal') {
+
+    setTimeout(() => {
+      OpenAnimation(modalId);
+      let modalElement = document.getElementById(modalId);
+      modalElement.classList.add("displayed");
+    }, 1000)
+  } else {
+    OpenAnimation(modalId);
+  }
+}
+
+function OpenAnimation(modalId) {
+  tl.to('#' + modalId, { x: '0' })
+        .from('.modal-container', { width: 0, height: 0 }, "-=1")
+        .to('#' + modalId, { backgroundColor: 'rgba(0, 0, 0, 0.35)' }, '-=0.25')
+        .from('#' + modalId + ' .seq', { y: -30, opacity: 0, stagger: 0.2, duration: 0.25 }, '-=1')
 }
